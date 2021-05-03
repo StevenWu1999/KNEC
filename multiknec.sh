8708d18c93c9e79cbb566359d1a85c435ff48b4e
吴震宇 Wu Zhenyu <2756679409@qq.com>
2021-05-03 21:54:11 +0800

    add multiknec.sh

---
+++ multiknec.sh
@@ -0,0 +1,36 @@
+
+
+
+echo "hello knec-parallel~"
+
+make clean
+make
+
+echo "finish building knec."
+
+parameters_dir="parameter_list"
+filelist=`ls $parameters_dir`
+
+
+for file in $filelist
+do  
+
+    echo 'begin running knec for '$file
+    outdir='Data_'$file
+    mkdir $outdir
+
+    screenfile='screen_output_'$file'.txt'
+    :> $screenfile
+
+    ./snec 'parameter_list/'$file > $screenfile 
+    mv $screenfile $outdir
+
+done
+
+
+
+
+
+
+
+
